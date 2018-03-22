d4ce0976f80f392331d909d20cefe67cc7bad1d4
Pedrobusou <pedrobusou@gmail.com>
2018-03-23 00:48:55 +0100

    Enemies detect target: Inside view (field and distance) When close When inside a "sound" sphere.
    Others:
    Added code documentation comments
    Added voice sounds

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+	[SerializeField]
+	Behaviour[] componentsToDisable;
+
+	Camera sceneCamera;
+
+	void Start() {
+		if (!this.isLocalPlayer) {
+			foreach (Behaviour component in this.componentsToDisable)
+				component.enabled = false;
+		} else {
+			this.sceneCamera = Camera.main;
+			if (this.sceneCamera)
+				this.sceneCamera.gameObject.SetActive(false);
+		}
+	}
+
+	void OnDisable() {
+		if (sceneCamera)
+			sceneCamera.gameObject.SetActive(true);
+	}
+}
