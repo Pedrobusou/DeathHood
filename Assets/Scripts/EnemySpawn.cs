5aa8e39e740c1a57f3976696a859d92acf4f7aad
Pedro Ramos González <pedrobusou@gmail.com>
2018-05-07 13:51:01 +0200

    Enemy ai (#7)
    * Set up of basic Keyboard and XboxOne controlls
    Changed ground plane by Terrain
    Now using Unity 2018.1
    
    * Day and night basic cycle
    
    * Automatic enemy spawn in time

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -3,52 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class EnemySpawn : NetworkBehaviour
-{
+public class EnemySpawn : NetworkBehaviour {
     [SerializeField] private GameObject objectToSpawn;
-    [SerializeField] private Transform spawnPoint;
-    [SerializeField] private bool RTInUse = false;
+
+    [Tooltip("Time (s) between enemy spawns")]
+    [SerializeField] private float timeBetweenSpawns = 30f;
+    [SerializeField] private float timeUntilNextSpawn;
+
+    private Transform spawnPoint;
+    [SerializeField] private List<Transform> enemySpawns;
+
+    private void Start() {
+        getEnemySpawns();
+        timeUntilNextSpawn = timeBetweenSpawns;
+    }
 
     // Update is called once per frame
-    private void Update()
-    {
-        if (!isLocalPlayer) { return; }
-        rtButton();
+    private void Update() {
+        //if (!isLocalPlayer) { return; }
+        spawnEnemy();
     }
 
     /// <summary>
-    /// Makes the RT button to work as a digital input instead of as an axis
+    /// Spawns an enemy each given time
     /// </summary>
-    private void rtButton()
-    {
-        if (!RTInUse)
-        {
-            if (InputManager.RTTrigger() == 1f)
-            {
-                print("RT Value: " + Input.GetAxis("RT_Button"));
-
-                if (RTInUse == false)
-                {
-                    RTInUse = !RTInUse;
-                    print("RT Trigger pressed");
-                    CmdSpawn();
-                }
-            }
+    private void getEnemySpawns() {
+        foreach (GameObject enemySpawn in GameObject.FindGameObjectsWithTag("EnemySpawn")) {
+            enemySpawns.Add(enemySpawn.transform);
         }
+    }
 
-        if (RTInUse)
-        {
-            if (InputManager.RTTrigger() <= 0.5f)
-            {
-                RTInUse = !RTInUse;
-                print("RT Trigger Released");
-            }
+    /// <summary>
+    /// Spawns an enemy each given time
+    /// </summary>
+    private void spawnEnemy() {
+        timeUntilNextSpawn -= 1 * Time.deltaTime;
+
+        if (timeUntilNextSpawn <= 0) {
+            timeUntilNextSpawn = timeBetweenSpawns;
+            print("Spawn points lenght: " + enemySpawns.Count);
+
+            //Get Random spawnpoint;
+            spawnPoint = enemySpawns[Random.Range(0, enemySpawns.Count)];
+
+            CmdSpawn();
         }
     }
 
     [Command]
-    private void CmdSpawn()
-    {
+    private void CmdSpawn() {
         GameObject go = Instantiate(objectToSpawn, spawnPoint);
         NetworkServer.Spawn(go);
     }
